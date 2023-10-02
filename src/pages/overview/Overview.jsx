c187f421f6e497190f6c6ccc7950d290d7ca0eaf
Ruud0511 <post@ruud.co>
2023-10-02 19:38:54 +0200

    data return aangemaakt

--- src/pages/overview/Overview.jsx
+++ src/pages/overview/Overview.jsx
@@ -1,9 +1,19 @@
 import './Overview.css'
 import {useParams} from "react-router-dom";
+import Blogpost from "../../constants/blogpost/Blogpost.jsx";
+
 function Overview () {
+
     const {postId} = useParams()
     return (
+        <>
         <h1>Hier lees je bericht {postId}</h1>
+        <div>
+            <span>
+                    <Blogpost/>
+            </span>
+        </div>
+        </>
     )
 }
 
